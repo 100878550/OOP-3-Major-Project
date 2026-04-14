eb82b39636191ad4d5c97e0867d7511d3829662b
Error80 <122498961+Error80@users.noreply.github.com>
2026-04-14 18:06:53 -0400

    Removed Debug Lines

--- MajorProject/src/Game/Main.java
+++ MajorProject/src/Game/Main.java
@@ -2,9 +2,6 @@ package Game;
 
 public class Main {
     public static void main(String[] args) {
-    	
-    	JsonHelper.WriteJSON(4, 39, 28);
-    	System.out.println(JsonHelper.ReadJSON());
         new GameFrame();
     }
 }
